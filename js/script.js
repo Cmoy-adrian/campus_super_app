4c9d9010a65238b6298cd614600fd30012adc66a
Cmoy07 <cmoyer07@comcast.net>
2025-11-05 09:49:17 -0500

    Added FullCalendar elements

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//Calendar Initialization
+document.addEventListener('DOMContentLoaded', function() {
+
+    const calendarEl = document.getElementById('event_calendar');
+
+    const calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        events: [ //Future Events added here for testing
+            { title: 'Math Club Meeting', date: '2025-11-15' },
+            { title: 'Basketball Game', date: '2025-11-21' }
+        ]
+    });
+
+    calendar.render();
+});
