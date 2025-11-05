db7ed9e4a5a87ea28bc8b422cc8e24e12bc92e0b
Cmoy07 <cmoyer07@comcast.net>
2025-11-05 10:11:18 -0500

    New FullCalendar functionality

--- js/script.js
+++ js/script.js
@@ -5,6 +5,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'dayGridMonth',
+    headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    },
+    buttonText: {
+        today: 'Today',
+        month: 'Month',
+        week: 'Week',
+        day: 'Day'
+    },
+        
         events: [ //Future Events added here for testing
             { title: 'Math Club Meeting', date: '2025-11-15' },
             { title: 'Basketball Game', date: '2025-11-21' }
